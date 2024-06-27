4e3337396a2d0fbc3c754b45180876d4167d0d39
tanuki <tanuki.evil@yandex.ru>
2024-06-28 01:18:49 +0300

    update build script

--- build.sh
+++ build.sh
@@ -3,14 +3,10 @@
 # Postgres позволяет подключиться к удаленной базе указав ссылку на нее после флага -d
 # ссылка подгрузится из переменной окружения, которую нам нужно будет указать на сервисе деплоя
 # дальше мы загружаем в поключенную базу наш sql-файл с таблицами
-# Exit on error
-set -o errexit
 
 source .env
 make install && psql -a -d $DATABASE_URL
 
-# Convert static asset files
-python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 make migrate
